e0d0fbd4a5d86b16d8b80a6fc9de97115871a608
MuhEid96 <moh.eid.1996@gmail.com>
2025-01-28 13:56:32 +0200

    feat: update app to run on all interfaces instead of localhost

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def download_pdf(filename):
         return jsonify({"error": "File not found"}), 404
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
